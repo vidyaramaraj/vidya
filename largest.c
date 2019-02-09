a9795ce7fb9bce4bea43418137d39a159e1cfdee
vidyaramaraj <47457052+vidyaramaraj@users.noreply.github.com>
2019-02-09 16:42:25 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+int main()
+
+
+{
+int a,b,c;
+
+scanf("%d %d %d",&a,&b,&c);
+
+{
+    
+if(a>b && a>c)
+   
+ {
+   
+ printf(" %d ,a");
+
+}
+
+else if(a==c)
+
+printf("%d",a);
+
+ if(b>a && b>c)
+
+ {
+    printf(" %d ,b");
+
+}
+
+else if(b==c)
+
+printf("%d",b);
+
+
+else
+
+{
+    printf("%d",c);
+
+}
+return 0;
+
+}}
